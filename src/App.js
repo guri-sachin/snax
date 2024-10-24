d4e2359872734d4adce43c83e184d87ae2d61552
guri-sachin <107688069+guri-sachin@users.noreply.github.com>
2024-10-24 15:02:31 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,123 @@
+import React, { useEffect } from 'react';
+import "./App.css";
+import { BrowserRouter as Router, Route, Routes, useLocation } from 'react-router-dom';
+import Homepage from "./pages/Homepage/Homepage";
+import Navbar from "./component/Navbar";
+import Premium from "./component/Premium";
+import Collection from "./component/Collection";
+import Sugarfree from "./component/SugarFree";
+import Customise from "./component/Customise";
+import Footer from "./component/Footer";
+import Product from "./pages/Homepage/Product";
+import Card from "./pages/Homepage/Card";
+import Contactus from "./pages/Homepage/Contactus";
+import Myorders from "./pages/Homepage/Myorders";
+import Signup from "./pages/Homepage/Signup";
+import Login from "./pages/Homepage/Login";
+import Payment from "./pages/Homepage/Payment";
+import Reviews from "./component/Reviews";
+import CodesForGift from "./component/CodesForGift";
+
+import FooterSection from "./component/FooterSection";
+import CustRiview from "./component/CustRiview";
+import Trending from "./component/Trending";
+import AddRiview from "./component/AddRiview";
+import Codes from "./component/Codes";
+import PrivecyPolicy from "./pages/Homepage/PrivecyPolicy";
+import Termsconditions from "./pages/Homepage/Terms&conditions";
+import RefuncPolicy from "./pages/Homepage/RefuncPolicy";
+import ShippingPolicy from "./pages/Homepage/ShippingPolicy";
+import Chocolate from "./pages/Homepage/Choc";
+import ProductList from "./pages/Homepage/ProductList";
+import CustomizeGift from "./pages/Homepage/CustomizeGift";
+import Gifthamper from "./pages/Homepage/Gifthamper";
+
+import Checkout from "./pages/Homepage/Checkout";
+import Checkouts from "./pages/Homepage/Checkouts";
+import Gifts from "./pages/Homepage/Gifts";
+import CustomItem from "./pages/Homepage/CustomItem";
+import LoginOtp from "./pages/Homepage/LoginOtp";
+import Aboutus from "./pages/Homepage/Aboutus";
+import { CartProvider } from './CartContext'; // Adjust the path accordingly
+
+const useGoogleAnalytics = () => {
+  const location = useLocation();
+
+  useEffect(() => {
+    // Load the Google Analytics script dynamically
+    const script = document.createElement('script');
+    script.src = `https://www.googletagmanager.com/gtag/js?id=G-1TSRVS6QD5`;
+    script.async = true;
+    document.head.appendChild(script);
+
+    // Initialize Google Analytics
+    window.dataLayer = window.dataLayer || [];
+    function gtag() {
+      window.dataLayer.push(arguments);
+    }
+    gtag('js', new Date());
+    gtag('config', 'G-1TSRVS6QD5');
+
+    // Track page views on route change
+    gtag('config', 'G-1TSRVS6QD5', { page_path: location.pathname });
+  }, [location]); // Rerun useEffect on route changes
+};
+
+function App() {
+  return (
+    <Router>
+      <CartProvider>
+        {/* Call the custom hook inside the Router context */}
+        <GoogleAnalyticsWrapper />
+        <Routes>
+        
+          <Route index path="/Codes" element={<Codes />} />
+          <Route index path="/CodesForGift" element={<CodesForGift />} />
+          <Route index path="/Trending" element={<Trending />} />
+
+          
+          <Route index path="/Chocolate" element={<Chocolate />} />
+          <Route index path="/" element={<Homepage />} />
+          <Route index path="/Navbar" element={<Navbar />} />
+          <Route index path="/Reviews" element={<Reviews />} />
+          <Route index path="/AddRiview" element={<AddRiview />} />
+          <Route index path="/PrivecyPolicy" element={<PrivecyPolicy />} />
+          <Route index path="/Termsconditions" element={<Termsconditions />} />
+          <Route index path="/RefuncPolicy" element={<RefuncPolicy />} />
+          <Route index path="/ShippingPolicy" element={<ShippingPolicy />} />
+          <Route index path="/CustRiview" element={<CustRiview />} />
+          <Route index path="/Premium" element={<Premium />} />
+          <Route index path="/Collection" element={<Collection />} />
+          <Route index path="/Sugarfree" element={<Sugarfree />} />
+          <Route index path="/Customise" element={<Customise />} />
+          <Route index path="/Footer" element={<Footer />} />
+          <Route index path="/Product/:slug/:encryptedId" element={<Product />} />
+          <Route index path="/Card" element={<Card />} />
+          <Route index path="/Contactus" element={<Contactus />} />
+          <Route index path="/Myorders" element={<Myorders />} />
+          <Route index path="/ProductList/:category" element={<ProductList />} />
+          <Route index path="/Signup" element={<Signup />} />
+          <Route index path="/Login" element={<Login />} />
+          <Route index path="/CustomizeGift" element={<CustomizeGift />} />
+          <Route index path="/Gifthamper" element={<Gifthamper />} />
+      
+          <Route index path="/Checkout" element={<Checkout />} />
+          <Route index path="/LoginOtp" element={<LoginOtp />} />
+          <Route index path="/Gifts" element={<Gifts />} />
+          <Route index path="/CustomItem" element={<CustomItem />} />
+          <Route index path="/Aboutus" element={<Aboutus />} />
+          <Route index path="/Checkouts" element={<Checkouts />} />
+          <Route index path="/Payment" element={<Payment />} />
+          <Route index path="/FooterSection" element={<FooterSection />} />
+        </Routes>
+      </CartProvider>
+    </Router>
+  );
+}
+
+function GoogleAnalyticsWrapper() {
+  useGoogleAnalytics();  // Using the hook inside the Router context
+  return null;  // This component does not render anything
+}
+
+export default App;
---
+++ src/pages/Homepage/Aboutus.js
@@ -0,0 +1,177 @@
+import React, { useState,useEffect } from 'react';
+import Navbar from '../../component/Navbar'
+import Footer from '../../component/Footer'
+import Newsletter from '../../component/Newsletter'
+import { Helmet } from "react-helmet";
+
+
+const MyDesign = () => {
+
+  const [page, setPage] = useState('about'); // State to hold the page input
+  const [seoData, setSeoData] = useState([]); // State to hold the fetched SEO data
+  const [error, setError] = useState(null); // State to handle error messages
+  const apiUrl = process.env.REACT_APP_API_URL;
+
+ // useEffect to fetch SEO data when the 'page' state changes
+ useEffect(() => {
+  const fetchSeoData = async () => {
+    try {
+      const response = await fetch(`${apiUrl}seo/${page}`);
+      if (!response.ok) {
+        throw new Error('No data found for the given page');
+      }
+      const data = await response.json();
+      setSeoData(data[0]); // Assuming the first result contains the data
+      setError(null); // Clear any previous errors
+    } catch (err) {
+      setSeoData(null);
+      setError(err.message);
+    }
+  };
+
+  if (page) {
+    fetchSeoData(); // Call the fetch function when the page value changes
+  }
+}, [page]); // Dependency array ensures useEffect runs when 'page' changes
+
+
+
+  return (
+          <> 
+             <Helmet>
+         <title>{seoData.seoTitle}</title> 
+         <meta name="title" content={seoData.seoTitle} />
+         <meta name="description" content={seoData.seoDes} />
+            <meta name="keywords" content={seoData.seoKeyword} />
+       
+  
+            <meta name="robots" content="index, follow" />
+          </Helmet>
+                <Navbar />
+              <div class="image-container">
+  <img 
+            src={`${process.env.PUBLIC_URL}/img/home/combine1.webp`}
+
+
+    alt="Image 1" 
+    class="w-[100vw] md:h-[700px] object-cover"
+  />
+</div>
+    <div className="flex flex-col lg:flex-row justify-center items-center p-6 px-8  lg:p-12 gap-10">
+      {/* Left Section - Illustration */}
+      <div className="lg:w-1/3 w-full flex justify-center lg:justify-start mb-6 lg:mb-0">
+        <img
+   
+          src={`${process.env.PUBLIC_URL}/img/home/combine.webp`}
+
+          alt="Illustration"
+          className="w-full max-w-sm lg:max-w-full h-auto"
+        />
+      </div>
+
+      {/* Right Section - Text Content */}
+      <div className="lg:w-1/2 w-full">
+        <p className="text-gray-600 leading-relaxed mb-4">
+          As life shifted during the lockdown, my home became a place of reflection and rediscovery. 
+          With my father and two young daughters under one roof, I found myself searching for snacks 
+          that could cater to all generations. But more than food, I sought something meaningful—the 
+          connection and joy that come from shared moments around the table.
+        </p>
+
+        <p className="text-gray-600 leading-relaxed mb-4">
+          My thoughts turned to my sister. Though life had taken us down different paths, our bond had 
+          always been rooted in a shared love for food. Some of my most cherished memories were of us 
+          cooking together—her artistic approach to flavours blending effortlessly with my practicality. 
+          One afternoon, while reminiscing about those days, I called her. That conversation sparked a new 
+          journey for us both: <strong className='text-[#8A0404]'>SnaXup</strong>.
+        </p>
+
+        <p className="text-gray-600 leading-relaxed">
+          SnaXup was born not just from a desire to create healthy snacks, but from the longing to 
+          rekindle our connection. Her expertise in fusing traditional Indian flavours with modern twists 
+          perfectly complemented my focus on nourishment and balance. Together, we envisioned a brand that 
+          went beyond food, one that would embody the essence of family and celebrate the unique bond between sisters.
+        </p>
+      </div>
+      
+    </div>
+
+
+    <div className="flex flex-col lg:flex-row justify-center items-center p-6 px-8  lg:p-12 gap-10">
+
+          
+      {/* Right Section - Text Content */}
+      <div className="lg:w-1/2 w-full">
+        <p className="text-gray-600 leading-relaxed mb-4">
+        What makes SnaXup truly special is our commitment to **freshly preparing every snack after an order is placed**.
+         This isn't just about providing healthier options—it's about infusing each snack with care and attention, making 
+         sure every bite is as fresh as if it came straight from our kitchen to yours
+        </p>
+
+        <p className="text-gray-600 leading-relaxed mb-4">
+        As the festive season approached, we realised that this journey was more than just a business—it was a celebration of relationships,
+         family, and love. Our tagline, <strong className='text-[#8A0404]'>SnaXup LevelUp</strong>
+        is an invitation for families everywhere to elevate not only their snacking choices but their connections with one another. 
+        </p>
+
+        <p className="text-gray-600 leading-relaxed">
+        SnaXup isn't just about food—it's a tribute to sisterhood, to the bonds that endure, and to the joy of creating something 
+        meaningful together. Every bite carries the love, creativity, and passion that only sisters united by purpose can bring to life.
+        </p>
+      </div>
+      {/* Left Section - Illustration */}
+      <div className="lg:w-1/3 w-full flex justify-center lg:justify-start mb-6 lg:mb-0">
+        <img
+                  src={`${process.env.PUBLIC_URL}/img/home/combine2.webp`}
+
+          alt="Illustration"
+          className="w-full max-w-sm lg:max-w-full h-auto"
+        />
+      </div>
+
+      
+    </div>
+    <div className="flex flex-col lg:flex-row justify-center items-center p-6 px-8  lg:p-12 gap-10">
+      {/* Left Section - Illustration */}
+      <div className="lg:w-1/3 w-full flex justify-center lg:justify-start mb-6 lg:mb-0">
+        <img
+                          src={`${process.env.PUBLIC_URL}/img/home/combine3.webp`}
+
+          alt="Illustration"
+          className="w-full max-w-sm lg:max-w-full h-auto"
+        />
+      </div>
+
+      {/* Right Section - Text Content */}
+      <div className="lg:w-1/2 w-full">
+        <p className="text-gray-600 leading-relaxed mb-4">
+          As life shifted during the lockdown, my home became a place of reflection and rediscovery. 
+          With my father and two young daughters under one roof, I found myself searching for snacks 
+          that could cater to all generations. But more than food, I sought something meaningful—the 
+          connection and joy that come from shared moments around the table.
+        </p>
+
+        <p className="text-gray-600 leading-relaxed mb-4">
+          My thoughts turned to my sister. Though life had taken us down different paths, our bond had 
+          always been rooted in a shared love for food. Some of my most cherished memories were of us 
+          cooking together—her artistic approach to flavours blending effortlessly with my practicality. 
+          One afternoon, while reminiscing about those days, I called her. That conversation sparked a new 
+          journey for us both: <strong className='text-[#8A0404]'>SnaXup</strong>.
+        </p>
+
+        <p className="text-gray-600 leading-relaxed">
+          SnaXup was born not just from a desire to create healthy snacks, but from the longing to 
+          rekindle our connection. Her expertise in fusing traditional Indian flavours with modern twists 
+          perfectly complemented my focus on nourishment and balance. Together, we envisioned a brand that 
+          went beyond food, one that would embody the essence of family and celebrate the unique bond between sisters.
+        </p>
+      </div>
+      
+    </div>
+    <Newsletter/>
+    <Footer/>
+    </>
+  );
+};
+
+export default MyDesign;
